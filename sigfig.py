527bab334f118e77eeb028ed1a787be3b887724c
annayqho <annayqho@gmail.com>
2017-10-11 22:38:32 -0400

    add functions

--- sigfig.py
+++ sigfig.py
@@ -7,3 +7,17 @@ def round_sig(x, sig=2):
     elif x < 0:
         return -round(-x, sig-int(floor(log10(-x)))-1)
     return round(x, sig-int(floor(log10(x)))-1)
+
+
+def ndec(num):
+    dec = str(num).split('.')[-1]
+    return len(dec)
+
+
+def format_val(val, sig):
+    valrd = round_sig(val, 2) 
+    sigrd = np.round(sig, ndec(valrd))
+    val_str = str(valrd) + "$\pm$" + str(sigrd)
+    if val < 0:
+        val_str = ""
+    return val_str
